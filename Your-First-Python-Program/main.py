b3e64beb47e756316d80d1c611bf8cc89630a7d3
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2024-09-08 15:30:28 -0600

    Update main.py

--- Your-First-Python-Program/main.py
+++ Your-First-Python-Program/main.py
@@ -6,3 +6,12 @@
 #
 
 # This is my first Python program.
+#
+# Name tatumn chavez
+# Date 9/8/23
+# Your First Python Program
+# COSC 1010 
+#
+
+# This is my first Python program.
+print("Hello World!")
