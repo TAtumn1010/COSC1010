54d09441bb9100b24b1d2829978bf63279a90b9c
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2024-09-09 16:58:23 -0600

    Update main.py

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Name Tatumn chavez
+# Date9/9/24
 # Sales Prediction Programming Project
 # COSC 2409 DNT
 #
@@ -8,7 +8,8 @@
 # Variables to hold the sales total and the profit
 
 # Get the amount of projected sales.
-
+total_sales=float(input('Enter the projected sales:'))
 # Calculate the projected profit.
-
+Profit =total_sales * 0.23
 # Print the projected profit.
+print('The profit is $',Profit)
