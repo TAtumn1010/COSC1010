a467fa03ef5fdb57af63b4cf361d37e9b349130e
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2024-09-23 18:42:37 -0600

    Update main.py

--- Budget-Analysis/main.py
+++ Budget-Analysis/main.py
@@ -1,7 +1,34 @@
 #
-# Name
-# Date
+# Name Tatumn chavez 
+# Date 9/23/24
 # Budget Analysis Programming Project
 # COSC 2409 DNT
 #
-# Use comments liberally throughout the program. 
+# Use comments liberally throughout the program.
+#  
+# 
+
+#Get the budget for the month.
+budget = int(input('What is your budget for the month? '))
+
+# Initialize the accumulator. 
+expenditure = 0
+
+# Loop get the weekly expenes . 
+for weeks in range(1, 5):
+    total = int(input('Enter the expenses for week ' + str(weeks) + ': '))
+    expenditure += total
+
+# calculate the total budget used
+x = budget - expenditure
+
+#Print the budget used
+if expenditure < budget:
+    print('This month you saved:', x, 'dollars.')
+else:
+    print('This month you went over budget by:', abs(x), 'dollars.')
+
+
+
+
+
