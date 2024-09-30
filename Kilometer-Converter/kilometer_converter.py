4ff28485bf87dd7bbf779de38ea9a2fa65ebb873
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2024-09-30 16:08:38 -0600

    Update kilometer_converter.py

--- Kilometer-Converter/kilometer_converter.py
+++ Kilometer-Converter/kilometer_converter.py
@@ -1,7 +1,31 @@
 #
-# Name
-# Date
+# Name Tatumn chavez
+# Date 9/30/24
 # Kilometer Converter Programming Project
 # COSC 2409 DNT
 #
-# Use comments liberally throughout the program. 
+# Use comments liberally throughout the program.
+def main():
+    # Constants
+    def main():
+        CONVERSION_FACTOR = 0.6214
+        
+
+    # get the distance in kilometers.
+    kilometers = float(input('Enter a distance in kilometers: '))
+
+    # Display the distance converted to miles.
+    show_miles(kilometers)
+  
+def show_miles(km) :
+    # Calculate miles.
+    CONVERSION_FACTOR = 0.6214
+    miles = km * CONVERSION_FACTOR
+
+    # Display the miles.
+    print(km,'kilometers equals', miles,'miles.')
+
+main()
+
+
+
