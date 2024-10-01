e44d8f8eef3b5c98591cceb84faaa1330528739d
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2024-09-30 20:13:03 -0600

    Update main.py

--- Feet-to-Inches/main.py
+++ Feet-to-Inches/main.py
@@ -1,7 +1,32 @@
 #
-# Name
-# Date
+# Name Tatumn Chavez
+# Date 9/30/24
 # Feet to Inches Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program.
+
+
+def main():
+    # Constants
+    INCHES_CONVERSION_FACTOR = 12
+
+    # get the distance in feet.
+    feet = float(input('Enter the amount of feet: '))
+
+    # Display the feet converted to inches.
+    show_inches(feet, INCHES_CONVERSION_FACTOR)
+
+def show_inches(x, INCHES_CONVERSION_FACTOR):
+    # Calculate inches using the conversion factor provided as an argument.
+    inches = x * INCHES_CONVERSION_FACTOR
+    
+    # Display the inches.
+    print(x,'feet equals ',inches, 'inches.')
+
+main()
+
+
+  
+
+
