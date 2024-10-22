41a85f28d74df594026f81db73d6956755251214
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2024-10-21 20:22:25 -0600

    Update main.py

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,31 @@
+
 #
-# Name
-# Date
-# Average of Numbers Programming Project
+# Name Tatumn Chavez
+# Date21/10/24
+## Average of Numbers Programming Project
 # COSC 2409 DNT
 #
-# Use comments liberally throughout the program. 
+
+def calculate_average(file_name):
+    # Initialize the variables 
+    total = 0  
+    count = 0  
+    # Open file in read mode
+    with open(file_name, 'r') as file:
+        # Iterate  the file over each line
+        for line in file:
+# Convert the line to an integer
+          total += number  
+          number = int(line)  
+            total += number  
+            count += 1  
+
+    if count == 0:
+        return 0 
+# Calculate the average
+    average = total / count  
+    return average 
+
+file_name = 'numbers.txt'
+average = calculate_average(file_name)
+print("Average:", average)
