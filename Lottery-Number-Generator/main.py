af19adcaf74d7dd968af96a90cee69467db5185a
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2024-10-28 22:03:48 -0600

    Update main.py

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,20 @@
 #
-# Name
-# Date
+# Name Tatumn Chavez
+# Date 28/10/24
 # Lottery Number Generator Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+import random
+
+# Generate a seven-digit lottery number
+
+lottery_numbers = [random.randint(0, 9) for _ in range(7)]
+
+# Display the contents of the list
+
+for number in lottery_numbers:
+    print(number,)
+
+    
