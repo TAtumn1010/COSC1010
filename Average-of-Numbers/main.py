34f9c3c99f964e7d02a73c3ba54ea953bf9be6fb
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2024-11-06 21:26:19 -0700

    Update main.py

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,31 +1,46 @@
 
 #
 # Name Tatumn Chavez
-# Date21/10/24
+# Date21/11/6
 ## Average of Numbers Programming Project
 # COSC 2409 DNT
 #
 
-def calculate_average(file_name):
-    # Initialize the variables 
-    total = 0  
-    count = 0  
-    # Open file in read mode
-    with open(file_name, 'r') as file:
-        # Iterate  the file over each line
-        for line in file:
-# Convert the line to an integer
-          total += number  
-          number = int(line)  
-            total += number  
-            count += 1  
+#
+# Name Tatumn Chavez
+# Date21/10/24
+#
+# COSC 2409 DNT
+#
 
-    if count == 0:
-        return 0 
-# Calculate the average
-    average = total / count  
-    return average 
 
-file_name = 'numbers.txt'
-average = calculate_average(file_name)
-print("Average:", average)
+# Open the file 
+
+num_file = open('numbers.txt', 'r')
+
+
+# Read  the line from the file
+
+num1 = int(num_file.readline())
+num2 = int(num_file.readline())
+num3 = int(num_file.readline())
+
+#close the file
+
+num_file.close()
+
+
+# Calculate the sum  and avege of the three numbers
+
+sum = num1 + num2 + num3
+average = sum / 3
+
+
+# Print statement
+
+print(num1, num2, num3)
+print(average)
+
+
+
+
